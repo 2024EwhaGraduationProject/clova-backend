be0d16301e412a9e5fda43b15794b99f121c00a2
LeeJE12 <jjungeun2013@gmail.com>
2024-09-25 14:18:24 +0900

    fix: 도커 배포 위해 수정

--- clova/accounts/models.py
+++ clova/accounts/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(max_length=200)
     profile = models.ImageField(upload_to='%Y%m%d/', blank=True, null=True)
     luckypoint = models.IntegerField(default=0, blank=True, null=True)
 
-    def __str__ (self):
+    # groups와 user_permissions 필드를 수정
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',  # 충돌 방지를 위한 related_name
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',  # 충돌 방지를 위한 related_name
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+    def __str__(self):
         return self.nickname
